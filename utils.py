5dc7ba8266652800d876af3c793d0a3c0b07a90e
GurjarAditya <aditya.gurjar08@gmail.com>
2018-11-06 16:18:01 -0600

    Add initial vocab tokenization code
    Added constants, utils and vocab tokenization file

---
+++ utils.py
@@ -0,0 +1,12 @@
+import re
+from constants import *
+
+def normalize_string(s):
+	s = s.lower()
+	s = re.sub(r"([.!?])", r" \1", s)
+	s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+	return s
+
+
+def get_idx_sentence(voc, word_sentence):
+	return [voc.wrd2idx[word] for word in word_sentence.split(' ')] + [constants.EOS_token]
